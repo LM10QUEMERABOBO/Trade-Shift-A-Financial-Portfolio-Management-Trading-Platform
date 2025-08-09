71825317b66bc8de6a103d051e8b42a5bb273883
krish_soni <krishsoni198@gmail.com>
2025-08-09 17:06:29 +0530

    first commit

---
+++ backend/Tradeshift/src/test/java/com/example/Tradeshift/TradeshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tradeshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
